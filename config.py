0e74b58400f2153811ae76392c554c85d0f1c34d
sidkroy24 <87103726+sidkroy24@users.noreply.github.com>
2022-06-29 22:28:42 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+""" FLASK Application Configurations """
+
+TESTING = True
+FLASK_ENV = "development"
+SESSION_PERMANENT = False
+SESSION_TYPE = "filesystem"
+SECRET_KEY = "secret"
+TEMPLATES_AUTO_RELOAD = True
